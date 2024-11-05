4a886199d89e5e35f2d4d3e63352f386deba33ba
Fartbongo <ntrusclark@gmail.com>
2024-11-04 22:10:11 -0500

    making robo see it is punct

--- bot.js
+++ bot.js
@@ -52,6 +52,7 @@ function playNextInQueue() {
         })
         .catch((error) => {
             console.error(`Error playing note ${noteFile}: ${error.message}`);
+            // Skip the problematic file and continue the queue
             setTimeout(playNextInQueue, 300);
         });
 }
@@ -63,6 +64,8 @@ function playNoteFromMessage(message) {
             let noteFile = path.join(__dirname, charToNote[char]);
             console.log(`Queuing: ${noteFile}`);
             queue.push(noteFile);
+        } else {
+            console.warn(`Character ${char} not mapped to any sound.`);
         }
     }
     if (!isPlaying) {
